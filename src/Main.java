cad2ec3a3e4d4691c77e1638e61a0756e036d2fb
Njebs M <njabumacfoy.w@gmail.com>
2024-11-17 23:41:43 +0000

    Main tests [WIP] fix setCell

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import substances.gas.*;
+import substances.liquid.*;
+import substances.solid.movableSolid.*;
+import substances.solid.staticSolid.*;
+import system.CellMatrix;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        CellMatrix cellMatrix = CellMatrix.INSTANCE.getInstance();
+        cellMatrix.setSize(5,5);
+
+        System.out.println(cellMatrix);
+        cellMatrix.stepAll();
+        System.out.println(cellMatrix);
+
+        cellMatrix.setCell(new Copper(1,0));
+
+        System.out.println(cellMatrix);
+        cellMatrix.stepAll();
+        System.out.println(cellMatrix);
     }
 }
