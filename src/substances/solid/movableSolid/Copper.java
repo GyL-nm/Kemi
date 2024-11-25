d372a0dbde1254d410ec6bbabd0f2b288d5ccc03
Njebs M <njabumacfoy.w@gmail.com>
2024-11-25 05:15:30 +0000

    Static copper

--- src/substances/solid/movableSolid/Copper.java
+++ src/substances/solid/movableSolid/Copper.java
@@ -3,7 +3,7 @@ package substances.solid.movableSolid;
 import substances.SubstanceProperties;
 
 public class Copper extends MovableSolid {
-    final SubstanceProperties PROPERTIES = SubstanceProperties.COPPER;
+    final SubstanceProperties PROPERTIES = SubstanceProperties.COPPER_POWDER;
 
     public Copper() {
         properties = PROPERTIES;
