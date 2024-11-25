65f6136a901df2fd7b49c3826615910008fac380
Njebs M <njabumacfoy.w@gmail.com>
2024-11-25 05:41:09 +0000

    solid copper chloride

---
+++ src/substances/solid/movableSolid/CopperChloride.java
@@ -0,0 +1,11 @@
+package substances.solid.movableSolid;
+
+import substances.SubstanceProperties;
+
+public class CopperChloride extends MovableSolid {
+    final private SubstanceProperties PROPERTIES = SubstanceProperties.COPPER_HYDROXIDE_SOLID;
+
+    public CopperChloride() {
+        properties = PROPERTIES;
+    }
+}
