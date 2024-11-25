347810a136403cee1a6484859f83a4d7ac737b4c
Njebs M <njabumacfoy.w@gmail.com>
2024-11-25 05:39:14 +0000

    Chlorine reactions

--- src/substances/gas/Chlorine.java
+++ src/substances/gas/Chlorine.java
@@ -1,10 +1,28 @@
 package substances.gas;
 
+import reactions.ConditionType;
+import reactions.ReactionCondition;
+import substances.Empty;
+import substances.liquid.*;
+import substances.solid.movableSolid.*;
+import substances.solid.staticSolid.*;
+
+import reactions.Reaction;
+import reactions.ReactionType;
 import substances.SubstanceProperties;
+
+import java.util.ArrayList;
 
 public class Chlorine extends Gas {
     final private SubstanceProperties PROPERTIES = SubstanceProperties.CHLORINE;
     public Chlorine() {
         properties = PROPERTIES;
+
+        this.reactions = new ArrayList<>();
+        reactions.add(new Reaction( Hydrogen.class,
+                                    new ReactionType[]{ReactionType.REDOX,ReactionType.SYNTHESIS},
+                                    new Class[]{substances.liquid.HydrochloricAcid.class, Empty.class},
+                                    10.0,
+                                    new ReactionCondition[]{}));
     }
 }
