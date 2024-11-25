3f70fadd7698c3f5f1e62224c196a8c487a543ee
Njebs M <njabumacfoy.w@gmail.com>
2024-11-25 05:41:24 +0000

    copper chloride liquid reactions

--- src/substances/liquid/CopperChloride.java
+++ src/substances/liquid/CopperChloride.java
@@ -1,10 +1,35 @@
 package substances.liquid;
 
+import reactions.Reaction;
+import reactions.ReactionCondition;
+import reactions.ReactionType;
+import substances.Empty;
 import substances.SubstanceProperties;
+
+import java.util.ArrayList;
 
 public class CopperChloride extends Liquid {
     final SubstanceProperties PROPERTIES = SubstanceProperties.COPPER_CHLORIDE_LIQUID;
     public CopperChloride() {
         properties = PROPERTIES;
+
+        this.reactions = new ArrayList<>();
+        reactions.add(new Reaction( substances.liquid.SodiumHydroxide.class,
+                new ReactionType[]{ReactionType.DOUBLE_DISPLACEMENT},
+                new Class[]{substances.liquid.SodiumChloride.class, substances.solid.movableSolid.CopperHydroxide.class},
+                10.0,
+                new ReactionCondition[]{} ));
+
+        reactions.add(new Reaction( substances.solid.movableSolid.SodiumHydroxide.class,
+                new ReactionType[]{ReactionType.DOUBLE_DISPLACEMENT},
+                new Class[]{substances.liquid.SodiumChloride.class, substances.solid.movableSolid.CopperHydroxide.class},
+                10.0,
+                new ReactionCondition[]{} ));
+
+        reactions.add(new Reaction( substances.liquid.SulfuricAcid.class,
+                new ReactionType[]{ReactionType.NEUTRALISATION},
+                new Class[]{Water.class, substances.liquid.CopperSulphate.class},
+                10.0,
+                new ReactionCondition[]{} ));
     }
 }
