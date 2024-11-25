a5121af71ef7a46820ffddae32a4e5ee1aa53d20
Njebs M <njabumacfoy.w@gmail.com>
2024-11-25 05:40:37 +0000

    Oxygen reactions

--- src/substances/gas/Oxygen.java
+++ src/substances/gas/Oxygen.java
@@ -1,10 +1,26 @@
 package substances.gas;
 
+import reactions.Reaction;
+import reactions.ReactionCondition;
+import reactions.ReactionType;
+
+
+import substances.Empty;
+import substances.liquid.*;
 import substances.SubstanceProperties;
+
+import java.util.ArrayList;
 
 public class Oxygen extends Gas {
     final private SubstanceProperties PROPERTIES = SubstanceProperties.OXYGEN;
     public Oxygen() {
         properties = PROPERTIES;
+
+        this.reactions = new ArrayList<>();
+        reactions.add(new Reaction( Hydrogen.class,
+                new ReactionType[]{ReactionType.REDOX,ReactionType.SYNTHESIS},
+                new Class[]{Water.class, Water.class},
+                10.0,
+                new ReactionCondition[]{}));
     }
 }
